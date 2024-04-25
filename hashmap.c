1bba5202eedba7b0c567937ec95023756cede9f2
isidoraO <163463602+isidoraO@users.noreply.github.com>
2024-04-25 02:12:37 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -63,14 +63,19 @@ void insertMap(HashMap * map, char * key, void * value) {
 void enlarge(HashMap * map) {
   enlarge_called = 1; //no borrar (testing purposes)
 
-  /*long aux_cap = map->capacity;
+  long aux_cap = map->capacity;
   Pair** aux = (Pair**) malloc(sizeof(Pair *) * aux_cap);
-  aux = map->buckets;*/
+  aux = map->buckets;
 
   map->capacity = map->capacity * 2;
   map->size = 0;
   map->buckets = (Pair**) realloc(map->buckets,sizeof(Pair *) * map->capacity);
-  
+
+  for(long i = 0; i < aux_cap; i++)
+  {
+    if(aux[i] != NULL && aux[i]->key != NULL)
+      insertMap(map, aux[i]->key, aux[i]->value);
+  }
   
 }
 
