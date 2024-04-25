59e53d8bc78857db1120b0630142278b811529ea
isidoraO <163463602+isidoraO@users.noreply.github.com>
2024-04-25 02:15:37 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -67,8 +67,9 @@ void enlarge(HashMap * map) {
   Pair** aux = (Pair**) malloc(sizeof(Pair *) * aux_cap);
   aux = map->buckets;*/
 
-  Pair** newB = (Pair**) malloc(sizeof(Pair *) * map->capacity);
   map->capacity = map->capacity * 2;
+  Pair** newB = (Pair**) malloc(sizeof(Pair *) * map->capacity);
+  
   map->buckets = newB;
   map->size = 0;
 
